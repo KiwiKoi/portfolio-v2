adbe417dc531bee2f116a7464e33dbbfe67b6dad
Daniel <danielo.visage@gmail.com>
2021-10-24 16:24:10 +0200

    finish mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,53 @@
+import React, { useState, useEffect } from 'react'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
 import styles from '../styles/Navbar.module.scss'
 
 
 function NavbarComponent() {
 
+    const [expandNavbar, setExpandNavbar] = useState<string | null>(null);
+
+    useEffect(() => {
+        setExpandNavbar('');
+
+    }, [])
+    const navbarToggler = () => {
+        expandNavbar === null ? setExpandNavbar(styles.expandNavbar) : setExpandNavbar(null);
+    }
+
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navbarContent}>
-                <div className={styles.navbarIcon}>
-                    <FontAwesomeIcon className={styles.navbarToggle} icon={faBars} />
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.navbarContent}>
                     <div className={styles.navbarBrand}><img src='images/code-solid.svg' /><a >Daniel Visage</a></div>
+                    <button type="button" onClick={() => navbarToggler()} className={styles.navbarToggle}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+
+
+                    {!expandNavbar &&
+                        <div className={styles.navLinks}>
+                            <a className={styles.navLink} href="#skills">Skillset</a>
+                            <a className={styles.navLink} href="#portfolio">Portfolio</a>
+                            <a className={styles.navLink} href="#contact">Contact</a>
+                        </div>}
+
                 </div>
-                <div className={styles.navLinks}>
+
+
+            </nav>
+            <div className={`${styles.mobileMenu} ${expandNavbar}`}>
+                <div className={styles.mobileNavLinks}>
                     <a className={styles.navLink} href="#skills">Skillset</a>
                     <a className={styles.navLink} href="#portfolio">Portfolio</a>
                     <a className={styles.navLink} href="#contact">Contact</a>
                 </div>
+
             </div>
 
+        </>
 
-        </nav>
     )
 }
 
